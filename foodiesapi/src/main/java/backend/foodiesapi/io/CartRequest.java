2a8ebabc702c6e98c3f4818e3755d47d0bf51159
Shashank <shashankbagale56@gmail.com>
2026-02-10 02:19:26 +0530

    Add CartRequest.java

---
+++ foodiesapi/src/main/java/backend/foodiesapi/io/CartRequest.java
@@ -0,0 +1,20 @@
+package backend.foodiesapi.io;
+
+public class CartRequest {
+    private String foodId;
+
+    public CartRequest() {
+    }
+
+    public CartRequest(String foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(String foodId) {
+        this.foodId = foodId;
+    }
+}
